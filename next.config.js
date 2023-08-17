2a0fa6834cded11dc004b3dc8f7a4ceb69d8664e
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-08-17 12:06:39 -0300

    Adding support for UnoCSS with attributify

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const UnoCSS = require('@unocss/webpack').default
+
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.plugins.push(new UnoCSS())
+        return config
+    }
+}
 
 module.exports = nextConfig
