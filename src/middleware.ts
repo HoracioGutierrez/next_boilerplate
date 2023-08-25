898f3761c93308b3322bfa478661de2a7bcff04c
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-08-25 14:21:46 -0300

    Change cookie name in middleware redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
   async function middleware(req) {
-    const token = req.cookies.get('next-auth.session-token')
+    const token = req.cookies.get('next-auth.session-token') || req.cookies.get('__Secure-next-auth.session-token')
     const url = new URL(req.url)
     if (token) {
       if (url.pathname === '/login' || url.pathname === '/signup') {
